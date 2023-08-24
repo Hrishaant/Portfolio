fd41c63c61b8bbfce8ec126dbaa79a7a72a6502a
Hrishaant <124888812+Hrishaant@users.noreply.github.com>
2023-08-24 19:39:42 +0530

    Trying out git

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-  <h1> Hi Hrizzshaant </h1>
-  </div>
-</template>
+  <SiteHeader></SiteHeader>
+  <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+  <NuxtPage></NuxtPage>
+  </main>
+  <SiteFooter></SiteFooter>
+ </div>
+ </template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+<section>
+    <h1 class="text-5xl font-bold mt-20">Hello, I'm Hrishant Rajish</h1>
+    <p class="text-base text-gray-900 p-2 italic">Creating with data, AI, and web to redefine possibilities</p>
+</section>
+<section>
+    <h2 class="text-3xl font-bold m-8">Latest Blog Posts</h2>
+    
+</section>
+</template>
